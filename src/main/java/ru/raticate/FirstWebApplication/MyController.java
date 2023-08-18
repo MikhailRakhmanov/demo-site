f9407b43d289229e209fd0176432ae73fdf043dd
MikhailRakhmanov <raticate@vk.com>
2023-08-18 10:41:15 +0300

    nikita some func

--- src/main/java/ru/raticate/FirstWebApplication/MyController.java
+++ src/main/java/ru/raticate/FirstWebApplication/MyController.java
@@ -1,20 +1,38 @@
 package ru.raticate.FirstWebApplication;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MyController {
+
     @GetMapping("/julia")
-    String julia(){
+    String julia() {
         return "julia";
     }
+
     @GetMapping("/nikita")
-    String nikita(){
+    String nikita() {
         return "nikita";
     }
-    @GetMapping("/hello_page")
-    String helloPageShow(){
+
+    @GetMapping("/")
+    String helloPageShow() {
         return "hello_page";
+    }
+
+    String nikitaMood;
+
+    @PostMapping("/nikita/message")
+    String message(@RequestParam(value = "mood") String mood) {
+        nikitaMood = mood;
+        System.out.println(mood);
+        return "redirect:/nikita/info";
+    }
+
+    @GetMapping("/nikita/info")
+    String iUnderstandPage() {
+        return "understand";
     }
 }
